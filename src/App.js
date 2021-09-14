076a5a9c298c92b1c7ad502df1216a61b11e507a
Woohyun Park <woohyunpark.mail@gmail.com>
2021-09-14 19:15:50 +0900

    QUESTION: question about class vs functional component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
-import React, {Component} from 'react';
+import React, {useRef} from 'react';
+import ScrollBox from './ScrollBox';
 import './App.css';
-import ComponentPractice from './ComponentPractice';
 
-class App extends Component {
-  render(){
-    return (
-      <>
-        <ComponentPractice name={1}/>
-      </>
-    );
+const App = () => {
+  const scrollRef = useRef();
+
+  const handleClick = () => {
+    scrollRef.current.scrollToBottom();
   }
+
+  return (
+    <>
+      <ScrollBox ref={scrollRef}/>
+      <button onClick={handleClick}>
+        Click to Scroll
+      </button>
+    </>
+  );
 }
 
 export default App;
+
+// import React, { Component } from 'react';
+// import ScrollBox from './ScrollBox';
+// import './App.css';
+
+// class App extends Component{
+//   render(){
+//     return(
+//       <>
+//         <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+//         <button onClick={() => {
+//           this.scrollBox.scrollToBottom();
+//         }}/>
+//       </>
+//     )
+//   }
+// }
+
+// export default App;
--- src/ScrollBox.js
+++ src/ScrollBox.js
@@ -1,32 +1,68 @@
-import React, { Component } from "react";
+import React, { useImperativeHandle } from "react";
 
-class ScrollBox extends Component{
-    scrollToBottom = () => {
-        const {scrollHeight, clientHeight} = this.DOM;
-        this.DOM.scrollTop = scrollHeight - clientHeight;
-    }
-    render(){
-        const style = {
-            border: '1px solid black',
-            height: '300px',
-            width: '300px',
-            overflow: 'auto',
-            positon: 'relative'
-        };
-
-        const innerStyle = {
-            width: '100%',
-            height: '650px',
-            background: 'linear-gradient(white, black)'
+const ScrollBox = React.forwardRef((props, ref) => {
+    useImperativeHandle(ref, () => ({
+        scrollToBottom(){
+            alert("scrollToBottom");
+            // const {scrollHeight, clientHeight} = ref;
+            // ref.scrollTop = scrollHeight - clientHeight;
         }
-        return (
+    }))
+    const style = {
+        border: '1px solid black',
+        height: '300px',
+        width: '300px',
+        overflow: 'auto',
+        positon: 'relative'
+    };
+
+    const innerStyle = {
+        width: '100%',
+        height: '650px',
+        background: 'linear-gradient(white, black)'
+    }
+    return (
+        <>
             <div
                 style={style}
-                ref={(ref) => {this.DOM=ref}}>
+                ref={ref}>
                 <div style={innerStyle}/>
             </div>
-        )
-    }
-}
+        </>
+    )
+})
 
 export default ScrollBox;
+
+// import React, {Component} from 'react';
+
+// class ScrollBox extends Component{
+//     scrollToBottom = () => {
+//         const {scrollHeight, clientHeight} = this.box;
+//         this.box.scrollTop = scrollHeight - clientHeight;
+//     }
+//     render(){
+//         const style = {
+//             border: '1px solid black',
+//             height: '300px',
+//             width: '300px',
+//             overflow: 'auto',
+//             positon: 'relative'
+//         };
+    
+//         const innerStyle = {
+//             width: '100%',
+//             height: '650px',
+//             background: 'linear-gradient(white, black)'
+//         }
+//         return (
+//             <div
+//                 style={style}
+//                 ref={(ref) => {this.box=ref}}>
+//                 <div style={innerStyle}/>
+//             </div>
+//         )
+//     }
+// }
+
+// export default ScrollBox;
