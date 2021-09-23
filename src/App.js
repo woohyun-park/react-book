56cf6a6a1186e6859c7e214b01e4f9171c5b2e90
Woohyun Park <woohyunpark.mail@gmail.com>
2021-09-24 01:00:12 +0900

    PROCESS: 챕터 8 useMemo

--- src/App.js
+++ src/App.js
@@ -1,75 +1,21 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import LifeCycleSample from './LifeCycleSample';
-import ErrorBoundary from './ErrorBoundary';
+import Counter from './Counter';
 
-class App extends Component {
-	state = {
-		color: '#000000',
-	};
-
-	getRandomColor = () => {
-		return '#' + Math.floor(Math.random() * 16777215).toString(16);
-	};
-
-	handleClick = () => {
-		this.setState({
-			color: this.getRandomColor(),
-		});
-		console.log(this.getRandomColor());
-	};
-	render() {
-		return (
-			<>
-				<button onClick={this.handleClick}>랜덤 색상</button>
-				<ErrorBoundary>
-					<LifeCycleSample color={this.state.color} />
-				</ErrorBoundary>
-			</>
-		);
-	}
-}
+const App = () => {
+	const [visible, setVisible] = useState(true);
+	return (
+		<>
+			<button
+				onClick={() => {
+					setVisible(!visible);
+				}}
+			>
+				{visible ? '숨기기' : '보이기'}
+			</button>
+			{visible && <Counter />}
+		</>
+	);
+};
 
 export default App;
-
-// import React, {useRef} from 'react';
-// import ScrollBox from './ScrollBox';
-// import './App.css';
-
-// const App = () => {
-//   const scrollRef = useRef();
-
-//   const handleClick = () => {
-//     scrollRef.current.scrollToBottom();
-//   }
-
-//   return (
-//     <>
-//       <ScrollBox ref={scrollRef}/>
-//       <button onClick={handleClick}>
-//         Click to Scroll
-//       </button>
-//     </>
-//   );
-// }
-
-// export default App;
-
-// import React, { Component } from 'react';
-// import ScrollBox from './ScrollBox';
-// import './App.css';
-
-// class App extends Component{
-//   render(){
-//     return(
-//       <>
-//         <ScrollBox ref={(ref) => this.scrollBox=ref}/>
-//         <button onClick={() => {
-//           this.scrollBox.scrollToBottom();
-//         }}/>
-//       </>
-//     )
-//   }
-// }
-
-// export default App;
--- src/Counter.js
+++ src/Counter.js
@@ -1,40 +1,88 @@
-import React from 'react';
-import { Component } from 'react';
+// import React, { useState } from 'react';
+// import { useEffect } from 'react/cjs/react.development';
 
-class Counter extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			number: 0,
-			fixedNumber: 0,
-		};
-	}
-	render() {
-		const { number, fixedNumber } = this.state;
-		const increase = () => {
-			this.setState(
-				(prevState) => {
-					return {
-						number: prevState.number + 1,
-					};
-				},
-				() => {
-					this.setState((prevState) => {
-						return {
-							number: prevState.number + 1,
-						};
-					});
-				}
-			);
-		};
-		return (
-			<>
-				<h1>{number}</h1>
-				<h1>{fixedNumber}</h1>
-				<button onClick={increase}>+1</button>
-			</>
-		);
-	}
-}
+// const getAverage = (numbers) => {
+// 	console.log('getAverage');
+// 	if (numbers.length === 0) return 0;
+// 	const sum = numbers.reduce((a, b) => a + b);
+// 	return sum / numbers.length;
+// };
+
+// const Counter = () => {
+// 	const [list, setList] = useState([]);
+// 	const [number, setNumber] = useState('');
+// 	const [avg, setAvg] = useState(0);
+
+// 	useEffect(() => {
+// 		console.log('here');
+// 		setAvg(getAverage(list));
+// 	}, [list]);
+
+// 	const onChange = (e) => {
+// 		setNumber(e.target.value);
+// 	};
+
+// 	const onInsert = (e) => {
+// 		const nextList = list.concat(parseInt(number));
+// 		setList(nextList);
+// 		setNumber('');
+// 	};
+
+// 	// const avg = useMemo(() => getAverage(list), [list]);
+
+// 	return (
+// 		<>
+// 			<input value={number} onChange={onChange} />
+// 			<button onClick={onInsert}>등록</button>
+// 			<ul>
+// 				{list.map((value, index) => {
+// 					return <li key={index}>{value}</li>;
+// 				})}
+// 			</ul>
+// 			<div>{avg}</div>
+// 		</>
+// 	);
+// };
+
+// export default Counter;
+
+import React, { useState, useMemo } from 'react';
+
+const getAverage = (numbers) => {
+	console.log('getAverage');
+	if (numbers.length === 0) return 0;
+	const sum = numbers.reduce((a, b) => a + b);
+	return sum / numbers.length;
+};
+
+const Counter = () => {
+	const [list, setList] = useState([]);
+	const [number, setNumber] = useState('');
+
+	const onChange = (e) => {
+		setNumber(e.target.value);
+	};
+
+	const onInsert = (e) => {
+		const nextList = list.concat(parseInt(number));
+		setList(nextList);
+		setNumber('');
+	};
+
+	const avg = useMemo(() => getAverage(list), [list]);
+
+	return (
+		<>
+			<input value={number} onChange={onChange} />
+			<button onClick={onInsert}>등록</button>
+			<ul>
+				{list.map((value, index) => {
+					return <li key={index}>{value}</li>;
+				})}
+			</ul>
+			<div>{avg}</div>
+		</>
+	);
+};
 
 export default Counter;
